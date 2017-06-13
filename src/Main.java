ea7b8f848e1aaf658ac20ee42d4f656b2fd49a13
Tiger <Tiger@pentium-dc>
2017-06-13 22:55:18 +0300

    Add Cat and Sparrow classes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new Dog().voice());
+        System.out.println(new Sparrow().voice());
+        System.out.println(new Cat().voice());
+        
     }
     
 }
