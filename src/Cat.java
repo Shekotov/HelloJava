c108194d1025146102a384f25fd39afc3f188e99
Tiger <Tiger@pentium-dc>
2017-06-13 22:56:37 +0300

    Add Cat and Sparrow classes (fix)

---
+++ src/Cat.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tiger
+ */
+public class Cat {
+    public String voice() {
+        return "Meow!";
+    }
+}
